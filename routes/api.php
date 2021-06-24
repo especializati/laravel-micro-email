55aaa039fed7b84b6b347f422ebd100237855576
Carlos Ferreira <conta@especializati.com.br>
2021-06-24 15:57:53 -0300

    Named Queue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Jobs\CompanyCreated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/teste', function () {
-    CompanyCreated::dispatch('testestestes@email.com');
+    CompanyCreated::dispatch('testestestes@email.com')->onQueue('queue_email');
 
     return response()->json(['message' => 'success']);
 });
