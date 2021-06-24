3b96360a8b568b7e354c70c83b011804c543f66c
Carlos Ferreira <conta@especializati.com.br>
2021-06-24 15:53:19 -0300

    Send Email Welcome

---
+++ resources/views/mails/companies/welcome.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Olá!
+
+Seja muito bem vindo(a) ao nosso sistema
+
+Obrigado,<br>
+{{ config('app.name') }}
+@endcomponent
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Jobs\CompanyCreated;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/teste', function () {
+    CompanyCreated::dispatch('testestestes@email.com');
+
+    return response()->json(['message' => 'success']);
+});
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
